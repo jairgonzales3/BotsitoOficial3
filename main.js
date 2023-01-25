0cda4fd07889a4c66fbf290943ff5c42fa82468b
Jair Gonzales <108778872+jairgonzales3@users.noreply.github.com>
2023-01-24 23:59:30 -0500

    Update main.js

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ const { state, saveState } = useSingleFileAuthState(global.authFile)
 const connectionOptions = {
 printQRInTerminal: true,
 auth: state,
-browser: ['Ryu Bot','Firefox','1.0.0'],
+browser: ['Richetti Bot','Firefox','1.0.0'],
 }
 
 global.conn = makeWASocket(connectionOptions)
